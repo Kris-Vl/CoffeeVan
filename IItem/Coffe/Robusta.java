72121f589e6873404cf281abc0e7d291a98abbe3
Kris-Vl <114858847+Kris-Vl@users.noreply.github.com>
2022-11-13 21:41:24 +0200

    Create Robusta.java

---
+++ IItem/Coffe/Robusta.java
@@ -0,0 +1,12 @@
+package IItem.Coffe;
+
+/**
+ * Опис сорту Робуста
+ */
+public class Robusta extends Coffee {
+    public Robusta(CoffeCondition coffeCondition, CoffeWrap coffeWrap) {
+        super("Робуста",
+                coffeCondition,
+                coffeWrap);
+    }
+}
