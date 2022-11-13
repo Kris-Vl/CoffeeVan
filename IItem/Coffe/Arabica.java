a6c57cfa94dce89e93278b5a078b439a2deb08e7
Kris-Vl <114858847+Kris-Vl@users.noreply.github.com>
2022-11-13 21:37:23 +0200

    Create Arabica.java

---
+++ IItem/Coffe/Arabica.java
@@ -0,0 +1,12 @@
+package IItem.Coffe;
+
+/**
+ * Опис сорту Арабіка
+ */
+public class Arabica extends Coffee {
+    public Arabica(CoffeCondition coffeeCondition, CoffeWrap coffeeWrap) {
+        super("Арабіка",
+                coffeeCondition,
+                coffeeWrap);
+    }
+}
